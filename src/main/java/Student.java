52d4ac2e5013a3d02814a317ecb0b1ef452d471f
toluibiwoye <72495445+toluibiwoye@users.noreply.github.com>
2023-12-06 14:41:26 +0000

    Update Student.java

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -3,26 +3,14 @@
  */
 public class Student {
 
-    public enum AttendanceGrade {
-        ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD
-    }
+    public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
-        if (attendance < 0 || attendance > 100) {
-            throw new IllegalArgumentException("Attendance percentage must be between 0 and 100");
-        } else if (attendance == 0) {
-            return AttendanceGrade.ABSENT;
-        } else if (attendance < 30) {
-            return AttendanceGrade.VERY_POOR;
-            // Since the low boundary for average is 30, the condition should be adjusted here
-        } else if (attendance >= 30 && attendance < 70) { // Changed from < 50 to >= 30
-            return AttendanceGrade.AVERAGE;
-            // Since we adjusted the average range, we now start 'GOOD' from 70
-        } else if (attendance >= 70 && attendance < 90) {
-            return AttendanceGrade.GOOD;
-            // 'VERY_GOOD' starts from 90 and includes 100
-        } else {
-            return AttendanceGrade.VERY_GOOD;
-        }
+        if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+        if (attendance < 30) return AttendanceGrade.VERY_POOR;
+        if (attendance < 50) return AttendanceGrade.POOR;
+        if (attendance < 70) return AttendanceGrade.AVERAGE;
+        if (attendance <= 90) return AttendanceGrade.GOOD;
+        return AttendanceGrade.VERY_GOOD;
     }
 }
